5b5a27dd12be8215922edf1d303948d308a0c15e
Finding_nemo <kamalsingh1472004@gmail.com>
2025-09-15 23:34:14 +0530

    condition problem

---
+++ ifstatement.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int age;
+    cin >> age;
+
+    if (age >= 18)
+    {
+        cout << "your are adult" << age;
+    }
+    else
+    {
+        cout << "you are not under 18, age: " << age;
+    }
+
+    return 0;
+}
