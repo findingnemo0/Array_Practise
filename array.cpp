c3c7e8fb6e6206b752c59ef3a3abff5958c01a4f
Finding_nemo <kamalsingh1472004@gmail.com>
2025-09-15 23:01:36 +0530

    practise C++

---
+++ array.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x, y;
+    cout << "Kamal Basera" << "\n";
+    cout << "UX Designer" << endl;
+
+    cin >> x >> y;
+    cout << "value of x:" << x << endl
+         << "value of y:" << y;
+
+    return 0;
+}
