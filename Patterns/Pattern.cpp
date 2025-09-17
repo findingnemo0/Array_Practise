35c8000f8f38d3cadcc77563e277a30160442bab
Finding_nemo <kamalsingh1472004@gmail.com>
2025-09-17 12:58:21 +0530

    pattern 10

--- Patterns/Pattern.cpp
+++ Patterns/Pattern.cpp
@@ -123,15 +123,38 @@ void pattern8(int n)
     }
 }
 
-void pattern9(int n){
-
+void pattern9(int n)
+{
+    for (int i = 1; i <= 2 * n - 1; i++)
+    {
+        int stars = (i <= n) ? i : (2 * n - i);
+        for (int j = 1; j <= stars; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
 }
 
+void pattern10(int n)
+{
+    int start = 1;
+    for (int i = 1; i < n; i++)
+    {
+        start = (i % 2 == 0) ? 1 : 0;
+        for (int j = 1; j < i; j++)
+        {
+            cout << start;
+            start = 1 - start;
+        }
+        cout << endl;
+    }
+}
 
 int main()
 {
     int n;
     cin >> n;
-    pattern7(n);
+    pattern10(n);
     return 0;
 }
