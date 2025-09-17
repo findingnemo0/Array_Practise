4d5017c6e1935e47b408cf5e993a2a085a562bdb
Finding_nemo <kamalsingh1472004@gmail.com>
2025-09-17 23:43:38 +0530

    pattern 11 to 21

--- Patterns/Pattern.cpp
+++ Patterns/Pattern.cpp
@@ -151,10 +151,240 @@ void pattern10(int n)
     }
 }
 
+void pattern11(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        // for left number
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j << " ";
+        }
+
+        // for space
+        for (int j = 1; j <= (n - i) * 2; j++)
+        {
+            cout << "  "; // give 2 space here
+        }
+
+        // for right number
+        for (int j = i; j >= 1; j--)
+        {
+            cout << j << " ";
+        }
+
+        cout << endl;
+    }
+}
+
+void pattern12(int n)
+{
+    int num = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << num << " ";
+            num++;
+        }
+        cout << endl;
+    }
+}
+
+void pattern13(int n)
+{
+    for (int i = 0; i <= n; i++)
+    {
+        for (char ch = 'A'; ch <= 'A' + i; ch++)
+        {
+            cout << ch << " ";
+        }
+        cout << endl;
+    }
+}
+
+void pattern14(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (char ch = 'A'; ch <= 'A' + (n - i - 1); ch++)
+        {
+            cout << ch << " ";
+        }
+        cout << endl;
+    }
+}
+
+void pattern15(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        char ch = 'A' + i;
+        for (int j = 0; j <= i; j++)
+        {
+            cout << ch << " ";
+        }
+        cout << endl;
+    }
+}
+
+void pattern16(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        // left spaces
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+
+        // characters
+        char ch = 'A';
+        int breakpoint = (2 * i + 1) / 2;
+        for (int j = 1; j <= 2 * i + 1; j++)
+        {
+            cout << ch;
+            if (j <= breakpoint)
+                ch++;
+            else
+                ch--;
+        }
+
+        // right spaces (optional, not needed)
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+
+        cout << endl;
+    }
+}
+
+void pattern17(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (char ch = 'E' - i; ch <= 'E'; ch++)
+        {
+            cout << ch << " ";
+        }
+        cout << endl;
+    }
+}
+
+void pattern18(int n)
+{
+    // upper part
+    int iniS = 0;
+    for (int i = 0; i < n; i++)
+    {
+        // stars
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << "*";
+        }
+        // spaces
+        for (int j = 0; j < iniS; j++)
+        {
+            cout << " ";
+        }
+        // stars
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << "*";
+        }
+        iniS += 2;
+        cout << endl;
+    }
+
+    // lower part
+    iniS = 2 * n - 2;
+    for (int i = 0; i < n; i++)
+    {
+        // stars
+        for (int j = 1; j <= i + 1; j++)
+        {
+            cout << "*";
+        }
+        // spaces
+        for (int j = 0; j < iniS; j++)
+        {
+            cout << " ";
+        }
+        // stars
+        for (int j = 1; j <= i + 1; j++)
+        {
+            cout << "*";
+        }
+        iniS -= 2;
+        cout << endl;
+    }
+}
+
+void pattern19(int n)
+{
+    int spaces = 2 * n - 2;
+    for (int i = 1; i <= 2 * n - 1; i++)
+    {
+        int stars = i;
+        if (i > n)
+            stars = 2 * n - i;
+        // stars
+        for (int j = 1; j <= stars; j++)
+        {
+            cout << "*";
+        }
+        // spaces
+        for (int j = 1; j <= spaces; j++)
+        {
+            cout << " ";
+        }
+        // stars
+        for (int j = 1; j <= stars; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+        (i < n) ? spaces -= 2 : spaces += 2;
+    }
+}
+
+void pattern20(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == 0 || j == 0 || i == n - 1 || j == n - 1)
+            {
+                cout << "*";
+            }
+            else
+                cout << " ";
+        }
+        cout << endl;
+    }
+}
+
+void pattern21(int n)
+{
+    for (int i = 0; i < 2 * n - 1; i++)
+    {
+        for (int j = 0; j < 2 * n - 1; j++)
+        {
+            int top = i;
+            int left = j;
+            int right = (2 * n - 2) - j;
+            int down = (2 * n - 2) - i;
+            cout << (n - min(min(top, down), min(left, right)));
+        }
+        cout << endl;
+    }
+}
 int main()
 {
     int n;
     cin >> n;
-    pattern10(n);
+    pattern21(n);
     return 0;
 }
